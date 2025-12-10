1a427160d9ea740f0b3d8036d34907fbd51edd08
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-12-10 11:45:58 -0300

    feat(servicos): cria controller para serviços com tratamento erros

---
+++ src/modules/servicos/servico.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { ServicoController } from "./servico.controller";
+
+const router = Router();
+const controller = new ServicoController();
+
+router.post("/", controller.create);
+router.get("/", controller.findAll);
+router.get("/:id", controller.findOne);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.delete);
+
+export default router;
