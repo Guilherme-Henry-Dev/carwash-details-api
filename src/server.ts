0de227bbe0919bcd40162b40c6d62b04728d1e9f
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-12-10 11:45:34 -0300

    chore(server): registra rotas de serviços no servidor express

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import  express  from "express";
 import cors from "cors";
 import { env } from "./config/env";
 import routes from "./routes"
+import servicoRoutes from "./modules/servicos/servico.routes";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", routes);
+app.use("/servicos", servicoRoutes);
 
 app.listen(env.port, () =>{
     console.log(`Server running att http://localhost:${env.port}`)
