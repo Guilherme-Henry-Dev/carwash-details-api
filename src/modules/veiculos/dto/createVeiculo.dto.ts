67065967c36620b74553b7ee65cf06b71c61d597
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-12-10 14:51:33 -0300

    feat(veiculos): adiciona DTOs CreateVeiculoDTO e UpdateVeiculoDTO

---
+++ src/modules/veiculos/dto/createVeiculo.dto.ts
@@ -0,0 +1,5 @@
+export interface CreateVeiculoDTO {
+  clienteId: number;
+  placa: string;
+  modelo: string;
+}
---
+++ src/modules/veiculos/dto/updateVeiculos.dto.ts
@@ -0,0 +1,4 @@
+export interface UpdateVeiculoDTO {
+  placa?: string;
+  modelo?: string;
+}
