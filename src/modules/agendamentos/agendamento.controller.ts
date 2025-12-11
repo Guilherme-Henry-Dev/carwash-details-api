8948f0d7516b9bee761972acf28284d84ebaa9f9
Guilherme Herny <guilherme.henrydesigner@gmail.com>
2025-12-11 12:02:08 -0300

    feat(agendamentos): cria controller com respostas estruturadas e tratamento de erros

---
+++ src/modules/agendamentos/agendamento.controller.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { AgendamentoService } from "./agendamento.service";
+
+const service = new AgendamentoService();
+
+export class AgendamentoController {
+    async create (req: Request, res: Response) {
+        try {
+            const result = await service.create(req.body);
+            return res.status(201).json(result);
+        } catch (error: any) {
+            if ( error.message.includes("não encontrado") ){
+                return res.status(404).json({  message: error.message });
+            }
+            return res.status(500).json({  message: "Erro ao criar agendamento" })
+        }
+    }
+
+    async findAll (req: Request, res: Response) {
+        try {
+            const result = await service.findAll();
+            return res.status(200).json(result);
+        } catch {
+            return res.status(500).json({  message: "Erro ao listar agendamento" });
+        }
+    }
+
+    async findOne (req: Request, res: Response){
+        try {
+            const id = Number(req.params.id);
+            const result = await service.findOne(id);
+            if (!result) return res.status(404).json({ message: "Agendamento não encontrado" });
+            return res.json(result);
+        } catch {
+            return res.status(500).json({  message: "Erro ao listar agendamento" });
+        }
+    }
+
+    async updadete(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const result = await service.update(id, req.body);
+            return res.json(result);
+        } catch {
+            return res.status(400).json({ message: "Erro ao atualizar agendamento" });
+        }
+    }
+
+    async delete(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            await service.delete(id);
+            return res.status(204).send();
+        } catch {
+            return res.status(400).json({ message: "Erro ao excluir agendamento" });
+        }
+  }
+
+}
